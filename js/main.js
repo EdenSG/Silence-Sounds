eb3d7187b7a88cf11e53d2e7c5916c71a7f02ba2
Eden S.-G <edgro2693@gmail.com>
2014-12-20 19:14:19 -0500

    Add 404 page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(function() {
-    console.log("blah!");
-    $('#nav-trigger').on('tap', function(e) {
-        if ($(this).hasClass('open')) {
-            $(this).removeClass('open').addClass('close');
-            $('#main-nav').addClass('shown');
-            $('body').css('overflow', 'hidden');
-        } else {
-            $(this).removeClass('close').addClass('open');
-            $('#main-nav').removeClass('shown');
-            $('body').css('overflow', 'auto');
-        }
+    // console.log("blah!");
+    $('#nav-trigger').on('tap', function(e){
+        openCloseMenu();
     });
 });
+
+function openCloseMenu(e) {
+    if ($('#nav-trigger').hasClass('open')) {
+        $('#nav-trigger').removeClass('open').addClass('close');
+        $('#main-nav').addClass('shown');
+        $('body').css('overflow', 'hidden');
+    } else {
+        $('#nav-trigger').removeClass('close').addClass('open');
+        $('#main-nav').removeClass('shown');
+        $('body').css('overflow', 'auto');
+    }
+}
